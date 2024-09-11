ece791781b6ac9a0d1e8f70113de39209fbd005e
Raiyan Ahmed <ray38250968@gmail.com>
2024-09-11 16:20:02 +0000

    bleh

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ app.use((req, res, next) => {
 	next();
 });
 
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+
 app.use(
 	express.static("../dist"),
 	express.json({ limit: "50mb", extended: true, type: "application/json" }),
